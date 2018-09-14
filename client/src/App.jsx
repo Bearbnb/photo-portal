02a25b91f90860d937da1123afc028033bf5087d
mchi168 <michaelchi168@gmail.com>
2018-09-13 22:37:24 -0700

    front progress finish map

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class App extends Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			properties:data.properties,
+			property:data.properties[0]
+		}
+	}
+
+	render() {
+		return (
+			<div> Ello m8 </div>
+		)
+	}
+}
